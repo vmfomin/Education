0546002db2ae2181f40d4dd1041ed85cfc71d8a9
vmf0min <vlifom@yandex.ru>
2020-10-29 11:01:40 +0300

    Commit 1_module

---
+++ 1_module/cmake-build-debug/CMakeFiles/1_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1_module.exe"
+  "1_module.pdb"
+  "CMakeFiles/1_module.dir/main.cpp.o"
+  "lib1_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
