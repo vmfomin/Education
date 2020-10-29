d7fdffc6e89809872a2cb70873f938b705794d70
vmf0min <vlifom@yandex.ru>
2020-10-29 13:11:32 +0300

    last ccommit

--- Git_Learn/main.cpp
+++ Git_Learn/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello" << std::endl;
+    std::cout << "Some text" << std::endl;
+
+
     return 0;
 }
