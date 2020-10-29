c6760b3f8cc60e5184c480fc939b44b3afde5499
vmf0min <vlifom@yandex.ru>
2020-10-29 12:33:37 +0300

    1 Commit console

---
+++ Git_Learn/cmake-build-debug/CMakeFiles/Git_Learn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Git_Learn.dir/main.cpp.o"
+  "Git_Learn.exe"
+  "Git_Learn.pdb"
+  "libGit_Learn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Git_Learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Git_Learn/cmake-build-debug/CMakeFiles/Git_Learn.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Git_Learn.
+# This may be replaced when dependencies are built.
