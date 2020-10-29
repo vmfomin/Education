1b2ca945ce7b3fefcf4030042486e4a7372ec0c1
vmf0min <vlifom@yandex.ru>
2020-10-29 13:22:46 +0300

    изменения

---
+++ Git_L/cmake-build-debug/CMakeFiles/Git_L.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Git_L.dir/main.cpp.o"
+  "Git_L.exe"
+  "Git_L.pdb"
+  "libGit_L.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Git_L.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
