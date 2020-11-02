b0c11715b75ec78d193620e656434549aa436b3b
vmf0min <vlifom@yandex.ru>
2020-11-02 17:52:59 +0300

    Модуль 7. Задача 5. Кратность числа.

---
+++ 7_module/7.5_module/cmake-build-debug/CMakeFiles/7_5_module.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/d/Fomin/SkillBox/C++/Skillbox/7_module/7.5_module/main.cpp" "/cygdrive/d/Fomin/SkillBox/C++/Skillbox/7_module/7.5_module/cmake-build-debug/CMakeFiles/7_5_module.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
