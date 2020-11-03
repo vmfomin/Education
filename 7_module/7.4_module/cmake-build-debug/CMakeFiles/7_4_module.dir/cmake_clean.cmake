e85cb9ac834609e5ca3b2c789408dea73e2e9757
vmf0min <vlifom@yandex.ru>
2020-11-03 11:37:16 +0300

    Модуль 7. Задача 4. Калькулятор опыта.

---
+++ 7_module/7.4_module/cmake-build-debug/CMakeFiles/7_4_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "7_4_module.exe"
+  "7_4_module.pdb"
+  "CMakeFiles/7_4_module.dir/main.cpp.o"
+  "lib7_4_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7_4_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
