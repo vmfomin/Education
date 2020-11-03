3832aa978d79064283b0cb8ba8cfb64a7b5b94a8
vmf0min <vlifom@yandex.ru>
2020-11-03 14:39:03 +0300

    Модуль 7. Задача 8. Зарплата (необязательная).

---
+++ 7_module/7.8_module/cmake-build-debug/CMakeFiles/7_8_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "7_8_module.exe"
+  "7_8_module.pdb"
+  "CMakeFiles/7_8_module.dir/main.cpp.o"
+  "lib7_8_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7_8_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
