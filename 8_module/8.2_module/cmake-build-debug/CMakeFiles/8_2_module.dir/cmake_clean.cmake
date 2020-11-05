4156314e45e475d92d78b264144eea420cc91c28
vmf0min <vlifom@yandex.ru>
2020-11-05 15:28:20 +0300

    Модуль 8. Задача 2. Полет нормальный!

---
+++ 8_module/8.2_module/cmake-build-debug/CMakeFiles/8_2_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "8_2_module.exe"
+  "8_2_module.pdb"
+  "CMakeFiles/8_2_module.dir/main.cpp.o"
+  "lib8_2_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_2_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
