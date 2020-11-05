8530d2dd5c9bfb0e3549ab18bdafc59d1eae3e98
vmf0min <vlifom@yandex.ru>
2020-11-05 15:53:18 +0300

    Модуль 8. Задача 3. Майские!

---
+++ 8_module/8.3_module/cmake-build-debug/CMakeFiles/8_3_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "8_3_module.exe"
+  "8_3_module.pdb"
+  "CMakeFiles/8_3_module.dir/main.cpp.o"
+  "lib8_3_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_3_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
