966ee048d39a93c43c06dfd5dcf906667016b280
vmf0min <vlifom@yandex.ru>
2020-11-05 15:06:25 +0300

    Модуль 8. Задача 1. Военный фельдшер.

---
+++ 8_module/8.1_module/cmake-build-debug/CMakeFiles/8_1_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "8_1_module.exe"
+  "8_1_module.pdb"
+  "CMakeFiles/8_1_module.dir/main.cpp.o"
+  "lib8_1_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_1_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
