45ac99a08a9b686e3c9357d7815085ff6d82bae8
vmf0min <vlifom@yandex.ru>
2020-11-06 13:39:57 +0300

    Модуль 8. Задача 5. Склонение русских слов.

---
+++ 8_module/8.5_module/cmake-build-debug/CMakeFiles/8_5_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "8_5_module.exe"
+  "8_5_module.pdb"
+  "CMakeFiles/8_5_module.dir/main.cpp.o"
+  "lib8_5_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_5_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
