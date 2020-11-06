69735793bd6054c13a2f4d025f6b7deed0d7b44d
vmf0min <vlifom@yandex.ru>
2020-11-06 12:32:03 +0300

    Модуль 8. Задача 4. Майские — усложнение.

---
+++ 8_module/8.4_module/cmake-build-debug/CMakeFiles/8_4_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "8_4_module.exe"
+  "8_4_module.pdb"
+  "CMakeFiles/8_4_module.dir/main.cpp.o"
+  "lib8_4_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_4_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
