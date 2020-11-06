aafb8fc55772b9b031acc3029f22a2ee30d2e48f
vmf0min <vlifom@yandex.ru>
2020-11-06 17:00:49 +0300

    Модуль 8. Задача 8. Грустное совершеннолетие.

---
+++ 8_module/8.8_module/cmake-build-debug/CMakeFiles/8_8_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "8_8_module.exe"
+  "8_8_module.pdb"
+  "CMakeFiles/8_8_module.dir/main.cpp.o"
+  "lib8_8_module.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_8_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
