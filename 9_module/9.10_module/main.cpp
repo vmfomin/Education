4155caa1da47e0a06b2141bef9ce06b24e3fbb0b
vmf0min <vlifom@yandex.ru>
2020-11-13 15:50:58 +0300

    Модуль 9.Задача 10. Угадай число - наоборот.

---
+++ 9_module/9.10_module/main.cpp
@@ -0,0 +1,38 @@
+/// Задача 10. Угадай число - наоборот.
+/// Напишите программу, которая угадывает число, задуманное пользователем. Число
+/// загадывается в диапазоне от 0 до 63. Программа задает вопросы вида “ваше
+/// число больше такого-то?” и на основе ответов пользователя “да” или “нет”
+/// угадывает число.
+
+#include <iostream>
+
+int main() {
+    int minValue = 0;
+    int lastValue = 64;
+    int x;
+
+    std::cout << "Отвечайте \"да\" или \"нет\" в зависимотсти от числа.\nКак только программа"
+                 " отгадает -- ответьте \"отгадали!\"\n";
+    std::string answer;
+
+    bool isFound = false;
+    while (!isFound) {
+        x = rand() % lastValue;
+
+        if (x <= minValue || x == lastValue)
+            continue;
+
+        isFound = x == minValue || x == lastValue;
+
+        std::cout << "Ваше число больше? " << x << " ";
+        std::cin >> answer;
+
+        if ("да" == answer) {
+            minValue = x;
+        } else if ("нет" == answer) {
+            lastValue = x;
+        } else isFound = true;
+    }
+
+    return 0;
+}
