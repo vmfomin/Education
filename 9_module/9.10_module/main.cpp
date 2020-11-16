39230b6d6424abad13e1e6e5b43364eb77cc7345
vmf0min <vlifom@yandex.ru>
2020-11-16 14:47:54 +0300

    Модуль 9.Задача 10. Угадай число - наоборот.

--- 9_module/9.10_module/main.cpp
+++ 9_module/9.10_module/main.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 
 int main() {
+    #pragma region Старый код с использованием rand()
+/*
     int minValue = 0;
     int lastValue = 64;
     int x;
@@ -32,6 +34,55 @@ int main() {
         } else if ("нет" == answer) {
             lastValue = x;
         } else isFound = true;
+    }
+ */
+#pragma endregion
+
+    int questionNumber[]{10, 20, 30, 40, 50, 60};
+    int firstValue = 0, lastValue = -1;
+
+    std::cout << "Отвечайте \"y\" или \"n\" в зависимотсти от числа.\nКак только программа"
+                 " отгадает -- ответьте \"f\"\n";
+    std::string answer;
+
+    int counterNumbers = 0;
+    bool isFound = false;
+    bool isRange = false;
+    while (!isRange) {
+        isRange = firstValue < lastValue;
+
+        if (!isRange) {
+            std::cout << "Ваше число больше? " << questionNumber[counterNumbers] << " ";
+            std::cin >> answer;
+
+            if ("y" == answer) {
+                firstValue = questionNumber[counterNumbers];
+            } else if ("n" == answer) {
+                lastValue = questionNumber[counterNumbers];
+            }
+
+            counterNumbers++;
+            if (6 == counterNumbers)
+                counterNumbers = 0;
+        } else {
+            std::cout << "Нашли диапазон: от " << firstValue
+                      << " до " << lastValue << "\n";
+            isRange = true;
+        }
+    }
+
+    while (!isFound) {
+        std::cout << "Ваше число больше? " << firstValue << " ";
+        std::cin >> answer;
+
+        if ("y" == answer) {
+            firstValue++;
+        } else if ("n" == answer) {
+            lastValue--;
+        } else if ("f" == answer) {
+            isFound = true;
+            std::cout << "\nНашли.\n";
+        }
     }
 
     return 0;
