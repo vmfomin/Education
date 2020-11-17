2e64eccd4242d961f36bc272ff34678f6427693a
vmf0min <vlifom@yandex.ru>
2020-11-17 15:18:10 +0300

    Модуль 10. Задача 8. Автограф разработчика.

---
+++ 10_module/10.08_module/main.cpp
@@ -0,0 +1,38 @@
+/// Задача 8. Автограф разработчика.
+/// Напишите программу, которая печатает на экране ваше имя в прямоугольной рамке.
+/// Для вертикальных линий используйте символ вертикального штриха “|”, а для
+/// горизонтальных - дефис “-”.
+
+
+#include <iostream>
+
+int main() {
+    int16_t nameLength; // Длина символов в имени.
+    int16_t height = 3; // Высота по умолчанию.
+    std::string name;
+
+    // Заметил, что кириллица в 2 раза длиньше по длине
+    std::cout << "Введите имя разработчика на литинице: ";
+    std::cin >> name;
+
+    nameLength = name.length();
+
+    bool isWriteName = false; // Проверка записи имени в консоль.
+
+    for (int i = 0; i < height; i++) {
+        std::cout << "|";
+        for (int j = 0; j < nameLength; j++) {
+            if (i == 0 || i == height - 1)
+                std::cout << "-";
+            else if (!isWriteName) {
+                std::cout << name;
+                isWriteName = true;
+            }
+            if (j == nameLength - 1)
+                std::cout << "|";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
