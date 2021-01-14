93ee25888edf2d398e21a10be5247f6ecaa19125
vmf0min <vlifom@yandex.ru>
2021-01-14 15:11:10 +0300

    Module 15. Task 4. Fixed

--- C++/10_module/10.10/main.cpp
+++ C++/10_module/10.10/main.cpp
@@ -1,48 +1,51 @@
-/// Задача 10. Ёлочка.
-/// Напишите программу, которая выводит на экран равнобедренный треугольник,
-/// заполненный символами решётки ‘#”. Пусть высота треугольника вводится
-/// пользователем. Обеспечьте контроль ввода.
-///   #
-///  ###
-/// #####
+/* Задача 10. Ёлочка.
+Напишите программу, которая выводит на экран равнобедренный треугольник,
+заполненный символами решётки ‘#”. Пусть высота треугольника вводится
+пользователем. Обеспечьте контроль ввода.
+   #
+  ###
+ ##### */
 
 #include <iostream>
 
 int main() {
-    std::cout << "Введите высоту равнобедренного треугольника: ";
-    int16_t height{10};
-    std::cin >> height;
-    std::cout << "\n";
+  std::cout << "\e[0mВведите высоту равнобедренного треугольника: ";
+  int16_t height{10};
+  std::cin >> height;
+  std::cout << "\n";
 
-    int16_t baseTriangle = height * 2; // Основание равнобедренного треугольника.
-    int16_t centerTriangle = baseTriangle / 2;
+  int16_t baseTriangle = height * 2;  // Основание равнобедренного треугольника.
+  int16_t centerTriangle = baseTriangle / 2;
 
-    int16_t countSharps{};
+  int16_t countSharps{};
 
-    for (int i = 0; i < height; i++) {
-        // Высчитываю сколько должно быть # в строке
-        int16_t sharpInLine = i * 2 + 1;
-        for (int j = 0; j < baseTriangle; j++) {
-            if (j < centerTriangle - sharpInLine || j > centerTriangle + sharpInLine)
-                std::cout << " ";
-            else {
-                if (countSharps < sharpInLine)
-                    if (countSharps == 0 && i == 0) {
-                        std::cout << "\033[33;1m" << "*";
-                    } else if (countSharps == 0 || countSharps == sharpInLine - 1
-                               || countSharps % 4 == 0) {
-                        std::cout << "\033[31;6m" << "o";
-                    } else
-                        std::cout << "\033[36;1m" << "#" << "\033[0m";
-                else
-                    std::cout << " ";
-                countSharps++;
-            }
-        }
-        countSharps = 0;
-        centerTriangle++;
-        std::cout << "\n";
+  for (int i = 0; i < height; ++i) {
+    // Высчитываю сколько должно быть # в строке
+    int16_t sharpInLine = i * 2 + 1;
+    for (int j = 0; j < baseTriangle; ++j) {
+      if (j < centerTriangle - sharpInLine || j > centerTriangle + sharpInLine)
+        std::cout << " ";
+      else {
+        if (countSharps < sharpInLine)
+          if (countSharps == 0 && i == 0) {
+            std::cout << "\e[33;1m"
+                      << "*\e[0m";
+          } else if (countSharps == 0 || countSharps == sharpInLine - 1 ||
+                     countSharps % 4 == 0) {
+            std::cout << "\e[31;1m"
+                      << "o\e[0m";
+          } else
+            std::cout << "\e[36;1m"
+                      << "#\e[0m";
+        else
+          std::cout << " ";
+        countSharps++;
+      }
     }
+    countSharps = 0;
+    centerTriangle++;
+    std::cout << "\e[0m\n";
+  }
 
-    return 0;
+  return 0;
 }
--- C++/15_module/15.04/main.cpp
+++ C++/15_module/15.04/main.cpp
@@ -8,11 +8,18 @@
 переключается и выводит сообщение об ошибке. Если пользователь вводит 0,
 приёмник отключается. */
 
+#include <windows.h>
+
 #include <iostream>
 
+/**
+ * @brief  Проверка радиостанции на соответствие.
+ * @param  &index: выбранная радиостанция.
+ */
 bool isCorrect(const int &index) {
   if (index < 0 || index > 10) {
-    std::cerr << "Selected the wrong radio station!!!\n";
+    std::cerr << "\e[31;1m"  // ! Красный цвет для ошибки
+              << "Error: selected the wrong radio station!!!\n";
     return false;
   }
   return true;
@@ -24,22 +31,46 @@ int main() {
                            "Rock FM",      "Серебряный дождь", "Монте-Карло",
                            "Comedy Radio", "Energy",           "Европа Плюс",
                            "Love Радио"};
-  float frequencyChannel[10]{87.5f,  90.3f,  90.8f,  95.2f,  100.1f,
-                             102.1f, 102.5f, 104.2f, 106.2f, 106.6f};
+  float frequencyChannel[]{87.5f,  90.3f,  90.8f,  95.2f,  100.1f,
+                           102.1f, 102.5f, 104.2f, 106.2f, 106.6f};
 
-  std::cout << "List of available radio stations:\n";
-  for (size_t i{}; i < 10; ++i)
-    std::cout << radioNames[i] << " (" << frequencyChannel[i] << ")\n";
-
-  int select;  // Выбранная радиостанция.
+  // Очистка консоли перед следующим выбором радиостанции.
+  std::cout << "\e[2J";
+  float seconds;
+  bool isSeconds{};
   do {
-    std::cout << "\nSelect a radio station (1 - 10): ";
-    std::cin >> select;
-    --select;
-  } while (!isCorrect(select));
+    std::cout << "How often do I refresh the screen (seconds)? ";
+    std::cin >> seconds;
+    seconds *= 1000;
 
-  std::cout << "You have chosen this radio station " << radioNames[select]
-            << " on the wave " << frequencyChannel[select] << "\n";
+    if (seconds <= 0)
+      std::cerr << "\e[31;1m"  // ! Красный цвет для ошибки
+                << "Error: the time cannot be less than 0!!!\e[0m\n";
+    else
+      isSeconds = true;
+  } while (!isSeconds);
 
-  return 0;
+  while (true) {
+    std::cout << "\e[2JList of available radio stations:\n";
+    for (size_t i{}; i < 10; ++i)
+      std::cout << radioNames[i] << " (" << frequencyChannel[i] << ")\n";
+
+    int select;  // Выбранная радиостанция.
+    do {
+      std::cout << "\e[0m\nSelect a radio station (1 - 10): ";
+      std::cin >> select;
+      if (0 == select) {
+        std::cout << "\e[32;1mShutdown...\e[0m";
+        return 0;
+      }
+
+      --select;
+    } while (!isCorrect(select));
+
+    std::cout << "\e[36;1mYou have chosen this radio station \""
+              << radioNames[select] << "\" on the wave "
+              << frequencyChannel[select] << "\e[0m\n";
+
+    Sleep(seconds);  // Чтобы не стирались данные.
+  };
 }
