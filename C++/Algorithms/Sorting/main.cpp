f6c9810dbab06bd13e5562afe85656697547ddae
vmf0min <vlifom@yandex.ru>
2021-08-12 09:04:33 +0300

    Quick sorting. Refactored.

--- C++/Algorithms/Sorting/main.cpp
+++ C++/Algorithms/Sorting/main.cpp
@@ -15,12 +15,35 @@
 using std::vector;
 
 /**
+ * @brief     Quick sorting
+ * @tparam    T             Input class
+ * @param     begin         container begin
+ * @param     end           container end
+ */
+template <class T>
+constexpr void quickSort(T begin, T end) noexcept {
+  T i{begin};
+  T j{end};
+  auto pivot{std::max(*(begin + ((end - begin) >> 1)),
+                      std::max(*(begin), *(end - 1)))};
+
+  while (i < j) {
+    while (*(i) < pivot) ++i;
+    while (*(j) > pivot) --j;
+    if (i <= j) std::swap(*(i++), *(j--));
+  }
+
+  if (j > begin) quickSort(begin, j);
+  if (end > i) quickSort(i, end);
+}
+
+/**
  * @brief     Selection sorting
  * @tparam    T             type of vector
  * @param     array           sorting vector
  */
 template <class T>
-void selectSort(T& array) {
+constexpr void selectSort(T& array) {
   for (int i = array.size() - 1; i >= 0; --i) {
     int max_index{};
     for (int j{}; j <= i; ++j)
@@ -35,33 +58,10 @@ void selectSort(T& array) {
  * @param     array           sorting vector
  */
 template <class T>
-void bubbleSort(T& array) {
+constexpr void bubbleSort(T& array) {
   for (int i{}; i < array.size(); ++i)
     for (int j{}; j < array.size() - i - 1; ++j)
       if (array[j] > array[j + 1]) std::swap(array[j], array[j + 1]);
-}
-
-/**
- * @brief     Quick sorting
- * @tparam    T             Input class
- * @param     begin         container begin
- * @param     end           container end
- */
-template <class T>
-void quickSort(T begin, T end) noexcept {
-  T i{begin};
-  T j{end};
-  auto pivot{std::max(*(begin + ((end - begin) >> 1)),
-                      std::max(*(begin), *(end - 1)))};
-
-  while (i < j) {
-    while (*(i) < pivot) ++i;
-    while (*(j) > pivot) --j;
-    if (i <= j) std::swap(*(i++), *(j--));
-  }
-
-  if (j > begin) quickSort(begin, j);
-  if (end > i) quickSort(i, end);
 }
 
 int main() {
