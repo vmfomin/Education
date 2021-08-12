7ae3945ad22bcb98298a7eab53a651c617308d0a
vmf0min <vlifom@yandex.ru>
2021-08-12 12:35:06 +0300

    Module 15. Task 4. Refactored.

--- C++/15_module/15.04/main.cpp
+++ C++/15_module/15.04/main.cpp
@@ -19,52 +19,19 @@
 #include <iterator>
 #include <vector>
 
-/**
- * @brief     Quick sorting with using abs
- * @tparam    T             Input class
- * @param     array         sorting vector
- * @param     last          length of part
- */
-template <class T>
-void quickSort(T array, int64_t last) {
-  int64_t i{};
-  int64_t j{last};
-  auto pivot = std::abs(*(array + (last >> 1)));
-
-  while (i <= j) {
-    while (std::abs(*(array + i)) < pivot) ++i;
-    while (std::abs(*(array + j)) > pivot) --j;
-    if (i <= j) std::swap(*(array + i++), *(array + j--));
-  }
-  if (j > 0) quickSort(array, j);
-  if (last > i) quickSort(array + i, last - i);
-}
-
 int main() {
-  using std::cout;
-  using std::vector;
-  cout << "\x1b[2J";
+  std::cout << "\x1b[2J";
 
-  vector<int32_t> vec{-100, -50, -5, 1, 10, 15};
-  cout << "Before sorting:\n";
-  std::copy(vec.begin(), vec.end(), std::ostream_iterator<int32_t>(cout, " "));
+  std::vector<int32_t> vec{-100, -50, -5, 1, 10, 15};
+  std::cout << "Before sorting:\n";
+  std::copy(vec.begin(), vec.end(),
+            std::ostream_iterator<int32_t>(std::cout, " "));
 
-  cout << "\n\nSorting with my quickSort() function:\n";
-  vector<int32_t> vecQuickSort(vec);
-  quickSort(vecQuickSort.begin(), vecQuickSort.size() - 1);
-  std::copy(vecQuickSort.begin(), vecQuickSort.end(),
-            std::ostream_iterator<int32_t>(cout, " "));
+  std::cout << "\n\nSorting with std::sort() and lambda expresion:\n";
+  std::sort(vec.begin(), vec.end(),
+            [](int32_t a, int32_t b) { return std::abs(a) < std::abs(b); });
+  std::copy(vec.begin(), vec.end(),
+            std::ostream_iterator<int32_t>(std::cout, " "));
 
-  cout << "\n\nSorting with std::sort() and lambda expresion:\n";
-  vector<int32_t> vecLambda(vec);
-  std::sort(vecLambda.begin(), vecLambda.end(),
-            [](const int32_t& a, const int32_t& b) {
-              return std::abs(a) < std::abs(b);
-            });
-  std::copy(vecLambda.begin(), vecLambda.end(),
-            std::ostream_iterator<int32_t>(cout, " "));
-
-  cout << "\n";
-
-  return 0;
+  std::cout << "\n";
 }
