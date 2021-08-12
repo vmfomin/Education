e57e86ca692eeae22f2a31132bb4192a8dc0d691
vmf0min <vlifom@yandex.ru>
2021-08-12 11:30:52 +0300

    Module 15. Task 1. Refactored.

--- C++/15_module/15.01/main.cpp
+++ C++/15_module/15.01/main.cpp
@@ -15,32 +15,24 @@
  */
 
 #include <iostream>
+#include <limits>
 #include <vector>
 
-int main() {
-  using std::cout;
-  using std::vector;
-  cout << "\x1b[2J";
-
-  vector<int> vec{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-
-  int maxSum{};
-  int indexStart{};
-  int indexEnd{};
-  for (int i{}; i < vec.size(); ++i) {
-    for (int j{}; j < vec.size() - i; ++j) {
-      int sum{};
-      for (int k{j}; k <= vec.size() - i; ++k) sum += vec[k];
-      if (sum > maxSum) {
-        maxSum = sum;
-        indexStart = j;
-        indexEnd = vec.size() - i;
-      }
-    }
+int32_t MaxSubArray(std::vector<int>& nums) {
+  int32_t max_sum{INT_MIN};
+  int32_t sum{};
+  for (size_t i{}; i < nums.size(); ++i) {
+    sum += nums[i];
+    if (sum > max_sum) max_sum = sum;
+    if (sum < 0) sum = 0;
   }
 
-  cout << "Start index = " << indexStart << ". End index = " << indexEnd
-       << ". Sum = " << maxSum << ".\n";
+  return max_sum;
+}
 
-  return 0;
+int main() {
+  system("cls");
+  std::vector<int32_t> nums{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+  std::cout << "Sum = " << MaxSubArray(nums) << ".\n";
 }
